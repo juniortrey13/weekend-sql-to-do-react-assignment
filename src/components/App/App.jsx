e1189bbabb6e9a9b6709794f0987bf642aaf5984
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-12-07 14:03:15 -0600

    set my todo list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,12 @@ import {useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App () {
-  
+  const [ todoList, setTodoList ] = useState( [] );
+
   return (
     <div>
       <h1>TO DO APP</h1>
+      <p>{ JSON.stringify( todoList )}</p>
     </div>
   );
 
