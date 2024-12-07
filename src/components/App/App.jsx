453eaca6273d26d50616c458e4ff5a6130ce229c
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-12-07 14:34:33 -0600

    created a onclick function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -17,13 +17,17 @@ function App () { // I am initializing my variable as an empty array, this will 
       alert( 'error getting to do list' );
     })
   }
+
+  function toggleME( id ){
+    console.log( 'in toggleME', id );
+  }
   return (
     <div>
       <h1>TO DO APP</h1>
       <p>{ JSON.stringify( todoList )}</p>
       {
         todoList.map( ( item )=>(
-          <p>{item.name} <button>Toggle Complete</button></p>
+          <p key={ item.id }>{item.name} <button onClick={ ()=>{ toggleME( item.id ) }}>Toggle Complete</button></p>
         ))
       }
     </div>
