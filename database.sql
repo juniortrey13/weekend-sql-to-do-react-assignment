77dd9b745639aacbd7dc66cf6ac7916b2a556d6c
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-12-07 13:44:44 -0600

    created my database.sql and added the code to create my table, pull data, and added values into the table

---
+++ database.sql
@@ -0,0 +1,27 @@
+-- db "weekend-to-do-app'
+
+-- creating my database
+CREATE TABLE "todo" (
+	"id" SERIAL PRIMARY KEY,
+	"name" varchar(225),
+	"complete" boolean
+);
+
+-- inserting only ONE value into my database
+INSERT INTO "todo" ("name", "complete")
+VALUES ('Laundry', false);
+
+-- pulling all the data to be displayed on my database
+SELECT * from todo;
+
+-- Added multiple values into my database at once
+INSERT INTO
+	"todo" ("name", "complete")
+VALUES
+	('Groceries', false),
+	('Yard', false),
+	('Feed the dogs', false),
+	('Dishes', false),
+	('Meal Prep', false),
+	('Homework', false),
+	('Workout', false);
