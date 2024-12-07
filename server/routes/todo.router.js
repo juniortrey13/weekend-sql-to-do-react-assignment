3f4b5433b14d46404672abe9426da670ff18ee06
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-12-07 13:54:00 -0600

    created a base for my GET router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const pool = require('../modules/pool.js');
 
 // GET
-
+router.get( '/', (req, res )=>{
+    console.log( '/api/todo GET hit' );
+    res.send( 'meow' );
+})
 // POST
 
 // PUT
