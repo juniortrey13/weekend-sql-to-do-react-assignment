4131959d67fc9319197869c577349a1e8bbdcaba
Junior Avalos <junior.trey.avalos13@gmail.com>
2024-12-09 19:03:22 -0600

    added comments to my database.sql

--- database.sql
+++ database.sql
@@ -7,14 +7,13 @@ CREATE TABLE "todo" (
 	"complete" boolean
 );
 
--- inserting only ONE value into my database
-INSERT INTO "todo" ("name", "complete")
-VALUES ('Laundry', false);
+-- inserting only ONE value into my database (POST / Create )
+INSERT INTO "todo" ("name", "complete") VALUES ('Laundry', false);
 
--- pulling all the data to be displayed on my database
+-- pulling all the data to be displayed on my database (GET / Retrieve )
 SELECT * from todo;
 
--- Added multiple values into my database at once
+-- Added multiple values into my database at once 
 INSERT INTO
 	"todo" ("name", "complete")
 VALUES
@@ -26,8 +25,8 @@ VALUES
 	('Homework', false),
 	('Workout', false);
 
--- Updating something from FALSE to TRUE
+-- Updating something from FALSE to TRUE (PUT / Update)
 UPDATE "todo" SET complete =NOT complete WHERE "id"=2;
 
--- Deleting something from my table
+-- Deleting something from my table (DELETE / Delete)
 DELETE FROM "todo" WHERE id=3;
